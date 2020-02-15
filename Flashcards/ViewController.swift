68e325758f1667e6e8540ab914cac2d7d5810abb
amy <amytan265@gmail.com>
2020-02-15 14:19:50 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Amy Tan on 2/15/20.
+//  Copyright © 2020 Amy Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
